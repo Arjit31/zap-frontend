12af9c03ee3c86d78c4766011a5b21f25aca96bb
Arjit31 <apk20023110@gmail.com>
2025-11-24 22:20:24 +0530

    buttons

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import { useRouter } from "next/navigation"
 import { LinkButton } from "./buttons/LinkButton"
+import { PrimaryButton } from "./buttons/PriamryButton";
 
 export const Appbar = () => {
+    const router = useRouter();
     return (
-        <div className="flex border-b justify-between">
+        <div className="flex border-b justify-between items-center">
             <div>
                 Workflow
             </div>
-            <div>
-                <LinkButton onClick={() => {}}>Login</LinkButton>
+            <div className="flex">
+                <LinkButton onClick={() => {router.push('/login')}}>Login</LinkButton>
+                <PrimaryButton onClick={() => {router.push('/signup')}}>Signup</PrimaryButton>
             </div>
         </div>
     )
--- components/buttons/PriamryButton.tsx
+++ components/buttons/PriamryButton.tsx
@@ -1,3 +1,20 @@
-const PrimaryButton = () => {
-    
-}
+import { ReactNode } from "react";
+
+export const PrimaryButton = ({
+    children,
+    onClick,
+    size = "small",
+}: {
+    children: ReactNode;
+    onClick: () => void;
+    size?: "small" | "big";
+}) => {
+    return (
+        <div
+            className={`${
+                size === "small" ? "text-sm px-2 py-4" : "text-xl px-5 py-10"
+            } bg-amber-600 text-white cursor-pointer`}
+            onClick={onClick}
+        >{children}</div>
+    );
+};
